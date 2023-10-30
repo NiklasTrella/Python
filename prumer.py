8d76e4576703839ea9b03d2ed9d7e6485a219ec1
= <niklas.klinecky@gmail.com>
2023-10-30 14:36:27 +0100

    Changed prumer to soucet

--- prumer.py
+++ prumer.py
@@ -1,9 +1,17 @@
 import sys
 
+# def prumer():
+#     soucet = 0
+#     for cislo in sys.argv[1:]:
+#         soucet += int(cislo)
+#     print(soucet/len(sys.argv[1:]))
+
+# prumer()
+
 def prumer():
     soucet = 0
     for cislo in sys.argv[1:]:
         soucet += int(cislo)
-    print(soucet/len(sys.argv[1:]))
+    print(soucet)
 
 prumer()
